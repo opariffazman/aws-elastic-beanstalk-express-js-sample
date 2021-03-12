1491a0afc3b17f961f8606aa561f6e1293ba06fa
opariffazman <opariffazman@gmail.com>
2021-03-12 14:42:41 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Greetings witcher!'));
+app.get('/', (req, res) => res.send('Greetings witcher & and witcheress!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
